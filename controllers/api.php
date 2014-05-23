41cbddab5ba26a781ff09ec28380110d9e114faa
Vítor Santos <vhsantos91@gmail.com>
2014-05-23 15:08:00 +0100

    Initial Commit
    First implementation of almost all functions. Lots of testing and bug
    fixing needed.

---
+++ controllers/api.php
@@ -0,0 +1,243 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Example
+ *
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array.
+ *
+ * @package		CodeIgniter
+ * @subpackage	Rest Server
+ * @category	Controller
+ * @author		Phil Sturgeon
+ * @link		http://philsturgeon.co.uk/code/
+*/
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Api extends REST_Controller
+{
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('webmodel');
+	}
+	//Retorna uma promoção dado o id
+	function promo_get()
+    {
+        if(!$this->get('id_promo'))
+        {
+        	$this->response(NULL, 400);
+        }
+
+		$promo=$this->webmodel->getPromoById($this->get('id_promo'));        
+    	
+        if(is_null($promo))
+        {
+			$this->response(array('error' => 'User could not be found'), 404);            
+        }
+        else
+        {
+          $this->response($promo, 200); // 200 being the HTTP response code  
+        }
+    }
+	
+	//Retorna a informação resumida de todas as promoções
+	function promos_get()
+    {
+        $promos=$this->webmodel->getAllPromos();        
+    	
+        if(is_null($promos))
+        {
+			$this->response(array('error' => 'User could not be found'), 404);            
+        }
+        else
+        {
+          $this->response($promos, 200); // 200 being the HTTP response code  
+        }
+    }
+	
+	function highlightedpromos_get()
+    {
+        $promos=$this->webmodel->getPromotedPromos($this->get('id_promo'));        
+    	
+        if(is_null($promos))
+        {
+			$this->response(array('error' => 'User could not be found'), 404);            
+        }
+        else
+        {
+          $this->response($promos, 200); // 200 being the HTTP response code  
+        }
+    }
+	
+	function entity_get()
+    {
+        if(!$this->get('id_entidade'))
+        {
+        	$this->response(NULL, 400);
+        }
+
+		$entity=$this->webmodel->getEntityById($this->get('id_entidade'));        
+    	
+        if(is_null($entity))
+        {
+			$this->response(array('error' => 'User could not be found'), 404);            
+        }
+        else
+        {
+          $this->response($entity, 200); // 200 being the HTTP response code  
+        }
+    }
+    
+    function userpoints_get()
+    {
+		if(!$this->get('id_utilizador'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+        $pontos=$this->webmodel->getPointsByUser($this->get('id_utilizador'));
+		
+		if(is_null($pontos))
+        { 
+			//Algo correu mal, nunca devia entrar aqui.
+			$this->response(array('error' => 'Critical error'), 404); 
+		}
+		else
+		{			
+			$this->response(array('id_utilizador'=>$this->get('id_utilizador'),'pontos' => $pontos), 200); // 200 being the HTTP response code
+		}
+    }
+    
+    function userpoints_post()
+    {
+		if(!$this->post('id_utilizador')||!$this->post('pontos'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+    	$result=$this->webmodel->setPointsByUser( $this->post('id_utilizador'),$this->post('pontos') );
+		
+		if($result)
+		{
+			 $this->response(array('Result' => 'User points updated'), 200);
+		}
+		else
+		{
+			//Algo correu mal, nunca devia entrar aqui.
+			$this->response(array('error' => 'Critical error'), 404); 			
+		}
+		
+    }
+	
+    
+    function voucher_get()
+    {
+        if(!$this->get('id_voucher'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+		$voucher=$this->webmodel->getVoucherById($this->get('id_voucher'));
+		
+        if(is_null($voucher))
+        {
+            $this->response($voucher, 200); // 200 being the HTTP response code
+        }
+
+        else
+        {
+            $this->response(array('error' => 'Couldn\'t find the wanted voucher!'), 404);
+        }
+    }
+	
+	function uservouchers_get()
+    {
+		if(!$this->get('id_utilizador'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+        $vouchers=$this->webmodel->getVouchersByUser($this->get('id_utilizador'));        
+    	
+        if(is_null($vouchers))
+        {
+			$this->response(array('error' => 'User could not be found or doesn\'t have any vouchers'), 404);            
+        }
+        else
+        {
+          $this->response($vouchers, 200); // 200 being the HTTP response code  
+        }
+    }
+	
+	function userhistory_get()
+	{
+		if(!$this->get('id_utilizador'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+		$vouchers=$this->webmodel->getHistory($this->get('id_utilizador'));        
+    	
+        if(is_null($vouchers))
+        {
+			$this->response(array('error' => 'User could not be found or doesn\'t have any vouchers'), 404);            
+        }
+        else
+        {
+          $this->response($vouchers, 200); // 200 being the HTTP response code  
+        }
+	}
+	
+	function promosbycat_get()
+	{	
+		if(!$this->get('categoria'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+		$promos=$this->webmodel->getPromosByCat($this->get('categoria'));        
+    	
+        if(is_null($promos))
+        {
+			$this->response(array('error' => 'There isn\'t any promo in this category'), 404);            
+        }
+        else
+        {
+          $this->response($promos, 200); // 200 being the HTTP response code  
+        }
+		
+	}
+	
+	function buyvoucher_put()
+	{
+		//substr(md5(rand()), 0, 7);
+		if(!$this->put('id_utilizador')||!$this->put('id_promo'))
+        {
+        	$this->response(NULL, 400);
+        }
+		
+		$data=array(
+			'id_promo'=>$this->put('id_promo'),
+			'id_utilizador'=>$this->put('id_utilizador'),
+			'chave_validacao'=>substr(md5(rand()), 0, 10),
+		);
+	
+		$this->webmodel->buyVoucher($data);
+		$this->response(array('Success'=>'Voucher bought'), 200); // 200 being the HTTP response code 		
+	}	
+
+	public function send_post()
+	{
+		var_dump($this->request->body);
+	}
+
+
+	public function send_put()
+	{
+		var_dump($this->put('foo'));
+	}
+}
---
+++ models/webmodel.php
@@ -0,0 +1,293 @@
+<?php
+
+class Webmodel extends CI_Model {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	//Devolve informação completa sobre uma promoção
+	public function getPromoById($id)
+	{
+		$query=$this->db->get_where('promo', array('id_promo' => $id));		
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+					$sql = "SELECT nome FROM categoria WHERE id_categoria = (SELECT id_categoria FROM catpromo WHERE id_promo= ?)";
+					$query2=$this->db->query($sql, array($rows->id_promo));
+					$row = $query2->row_array();
+					$cat=$row['nome'];					
+				
+					$promo = array(
+					  'desc_completa'  => $rows->desc_completa,
+					  'inicio_promo'  => $rows->inicio_promo,
+					  'fim_promo'    => $rows->fim_promo,
+					  'valor_voucher'  => $rows->valor_voucher,
+					  'no_vouchers'    => $rows->no_vouchers,
+					  'destaque'	=>$rows->destaque,
+					  'id_promo'	=>$rows->id_promo,
+					  'fim_destaque'  => $rows->fim_destaque,
+					  'id_entidade'    => $rows->id_entidade,
+					  'desc_resumida'  => $rows->desc_resumida,
+					  'reserva'    => $rows->reserva,
+					  'condicoes'	=>$rows->condicoes,
+					  'parceria'=>$rows->parceria,
+					  'horario'  => $rows->horario,
+					  'poupanca'    => $rows->poupanca,
+					  'bilhete'    => $rows->bilhete,
+					  'hora_criacao'	=> $rows->hora_criacao,
+					  'categoria'    => $cat,
+					  'zonas'    => $rows->zonas		  
+					);	
+			}
+		}
+		return $promo;
+	}
+	
+	//Devolve informações resumidas de todas as promoções. Gastar menos dados.
+	public function getAllPromos()
+	{
+		$query=$this->db->get('promo');
+		
+		$data=array();
+		
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$sql = "SELECT nome FROM categoria WHERE id_categoria = (SELECT id_categoria FROM catpromo WHERE id_promo= ?)";
+				$query2=$this->db->query($sql, array($rows->id_promo));
+				$row = $query2->row_array();
+				$cat=$row['nome'];
+				
+				$promo = array(
+					  'inicio_promo'  => $rows->inicio_promo,
+					  'fim_promo'    => $rows->fim_promo,
+					  'valor_voucher'  => $rows->valor_voucher,
+					  'destaque'	=>$rows->destaque,
+					  'id_promo'	=>$rows->id_promo,
+					  'fim_destaque'  => $rows->fim_destaque,
+					  'id_entidade'    => $rows->id_entidade,
+					  'desc_resumida'  => $rows->desc_resumida,
+					  'categoria'    => $cat
+					);	
+				
+				array_push($data, $promo);
+			}
+			return $data;
+		}
+		else return NULL;
+		
+	}
+	
+	public function getPromotedPromos()
+	{
+		$promos=$this->db->get('promo');
+		$data=array();
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				if($rows->destaque==1)
+				{
+					$promo = array(
+					  'fim_promo'    => $rows->fim_promo,
+					  'valor_voucher'  => $rows->valor_voucher,
+					  'destaque'	=>$rows->destaque,
+					  'id_promo'	=>$rows->id_promo,
+					  'fim_destaque'  => $rows->fim_destaque,
+					  'id_entidade'    => $rows->id_entidade,
+					  'desc_resumida'  => $rows->desc_resumida,
+					  'categoria'    => $cat
+					);	
+				
+					array_push($data, $promo);
+				}
+			}
+			return $data;
+		}
+		else return NULL;		
+	}
+	
+	public function getEntityById($id)
+	{
+		$this->db->where("id_entidade",$id);		
+		$query=$this->db->get("entidade");
+		
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{	
+				$user = array(
+					  'id_entidade'  => $rows->id_entidade,
+					  'nome'  => $rows->nome,
+					  'email'    => $rows->email,
+					  'contacto'  => $rows->contacto,
+					  'descricao'    => $rows->descricao,
+					  'morada'	=>$rows->morada,
+					  'nome_prop'    => $rows->nome_prop,
+				);				
+				return $user;
+			}			
+		}
+		else return NULL;
+	}
+	
+	public function getPointsByUser($id)
+	{
+		$this->db->where("id_utilizador",$id);		
+		$query=$this->db->get("userpontos");
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$pontos=$rows->pontos;
+			}			
+		}
+		else
+		{
+			$this->setPointsByUser($id,0);
+			$pontos=0;			
+		}
+		return $pontos;
+	}
+	
+	public function setPointsByUser($id,$points)
+	{
+		$query=$this->db->get_where('userpontos', array('id_utilizador' => $id));
+		if($query->num_rows()>0)
+		{
+			$this->db->where('id_utilizador',$id);
+			$this->db->update('userpontos', array('pontos'=>$pontos));
+			return true;
+		}
+		else
+		{
+			$this->db->insert('userpontos',array('id_utilizador'=>$id,'pontos'=>$points));
+			return true;
+		}		
+	}
+	
+	public function getVoucherById($id)
+	{
+		$this->db->where("id_voucher",$id);		
+		$query=$this->db->get("voucher");
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$voucher = array(
+					  'chave_validacao'  => $rows->chave_validacao,
+					  'data_validacao'  => $rows->data_validacao,
+					  'estado'    => $rows->estado,
+					  'id_promo'  => $rows->id_promo,
+					  'id_utilizador'    => $rows->id_utilizador,
+					  'id_voucher'	=>$rows->id_voucher					  
+				);
+				return $voucher;
+			}			
+		}
+		else return NULL;		
+	}
+	
+	//Devolve todos os vouchers.Será necessário?
+	public function getVouchers()
+	{		
+		$query=$this->db->get("voucher");
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$voucher = array(
+					  'chave_validacao'  => $rows->chave_validacao,
+					  'data_validacao'  => $rows->data_validacao,
+					  'data_compra'  => $rows->data_validacao,
+					  'estado'    => $rows->estado,
+					  'id_promo'  => $rows->id_promo,
+					  'id_utilizador'    => $rows->id_utilizador,
+					  'id_voucher'	=>$rows->id_voucher					  
+				);
+				array_push($data,$voucher);			
+			}
+			return $data;
+		}
+		else return NULL;	
+	}
+	
+	public function getVouchersByUser($id)
+	{
+		$this->db->where("id_utilizador",$id);	
+		$query=$this->db->get("voucher");
+		$data=array();
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$voucher = array(
+					  'chave_validacao'  => $rows->chave_validacao,
+					  'data_validacao'  => $rows->data_validacao,
+					  'data_compra'  => $rows->data_validacao,
+					  'estado'    => $rows->estado,
+					  'id_promo'  => $rows->id_promo,
+					  'id_utilizador'    => $rows->id_utilizador,
+					  'id_voucher'	=>$rows->id_voucher					  
+				);
+				array_push($data,$voucher);			
+			}
+			return $data;
+		}
+		else return NULL;	
+	}
+	
+	public function buyVoucher($data)
+	{
+		$this->db->insert('voucher',$data); 
+	}
+	
+	public function getHistory($id)
+	{
+		$this->db->where("estado","validado");	
+		$this->db->where("id_utilizador",$id);
+		$query=$this->db->get("voucher");
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$voucher = array(
+					  'chave_validacao'  => $rows->chave_validacao,
+					  'data_validacao'  => $rows->data_validacao,
+					  'data_compra'  => $rows->data_validacao,
+					  'estado'    => $rows->estado,
+					  'id_promo'  => $rows->id_promo,
+					  'id_utilizador'    => $rows->id_utilizador,
+					  'id_voucher'	=>$rows->id_voucher					  
+				);
+				array_push($data,$voucher);			
+			}
+			return $data;
+		}
+		else return NULL;
+	}
+	
+	public function getPromosByCat($category)
+	{
+		$sql = "SELECT id_categoria FROM categoria WHERE nome= ?";
+		$query2=$this->db->query($sql, array($category));
+		$row = $query2->row_array();
+		$id_cat=$row['id_categoria'];
+		
+		$data=array();
+		$query=$this->db->get_where('catpromo', array('id_categoria' => $id_cat));
+		if($query->num_rows()>0)
+		{
+			foreach($query->result() as $rows)
+			{
+				$promo=$this->getPromoById($rows->id_promo);
+				array_push($data,$promo);	
+			}
+		}
+		else return NULL;		
+	}
+}
